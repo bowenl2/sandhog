a112d10050e4f93d47bbbd5027bc9623f3f1992a
Liam Bowen <LiamBowen@gmail.com>
2014-03-12 05:39:33 -0400

    Initial commit

---
+++ sandhog.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	//"code.google.com/p/go.crypto/ssh"
+)
+
+func main() {
+	fmt.Println("sandhog: tunnel server")
+	// usage: sandhog [--log <log-path>] <listen-port>
+
+
+
+}
